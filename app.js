80f26b12cbbf14406e2f6aac60db72621f5c385e
AyusshVa <ayusshvashishth@gmail.com>
2022-04-17 16:08:13 +0530

    add database mongodb

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const { acceptsLanguages } = require("express/lib/request");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -17,11 +16,48 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-// global variables
-let postArr = [];
+// **************** mongoose databse code ********************
+
+// setting up mongoose server.
+mongoose.connect("mongodb://localhost:27017/blogDb");
+
+// create schema
+const blogSchema = new mongoose.Schema({
+  title: String, 
+  postContent: String
+});
+
+// create model 
+const blogModel = new mongoose.model("blogPost", blogSchema);
+
+// delete ->  ********* insert two random blogs and show in home.
+
+const blog1 = new blogModel({
+  title: "test1",
+  postContent: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis libero esse voluptatem deleniti, ipsa blanditiis adipisci, amet laboriosam voluptate assumenda debitis sit perferendis enim cum? Amet, sed. Cumque accusamus veniam voluptatibus asperiores, amet deserunt magni nostrum quasi aperiam numquam quas voluptates dolor, fugit in minima!"
+}); 
+// blog1.save();
+
+const blog2 = new blogModel({
+  title: "test2",
+  postContent: aboutContent
+})
+// blog2.save();
+
+
+
 
 app.get("/", (req,res)=>{
-  res.render("home",{content: homeStartingContent, arr: postArr});
+
+  blogModel.find((err, postArr)=>{
+    if(!err){
+      res.render("home",{content: homeStartingContent, arr: postArr});
+    }
+    else{
+      res.send("error")
+      console.log(err);
+    }
+  })
 })
 
 // about page. -------------
@@ -40,24 +76,26 @@ app.get("/compose" , (req,res)=>{
 
 // post for compose ...............................................
 app.post("/compose", (req,res)=>{
-var blogPost = {
+const  blogPost = new blogModel({
   title: req.body.title,
-  content: req.body.post
-}
-
-postArr.push(blogPost);
-res.redirect("/")
+  postContent: req.body.post
+});
+blogPost.save();
+  res.redirect("/")
 })
 
 // ********** for rendering posts ************
 app.get("/post/:newPost", (req,res)=>{
-  let paraVar = _.lowerCase(req.params.newPost)
-postArr.forEach((ele)=>{
-  let titleVar = _.lowerCase(ele.title)
-  if(titleVar === paraVar){
-    res.render("post", {content: ele.content, postTitle: ele.title})
-  }
-})
+  let paraVar = _.lowerCase(req.params.newPost);
+  blogModel.find((err,postArr)=>{
+    postArr.forEach((ele)=>{
+      let titleVar = _.lowerCase(ele.title)
+      if(titleVar === paraVar){
+        res.render("post", {content: ele.postContent, postTitle: ele.title})
+      }
+    });
+
+  })
 })
 
 app.listen(3000, function() {
